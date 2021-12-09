9fee21b077b7013a5f0e7743b72f4185c41408e6
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-12-09 12:41:46 +0200

    Roueter and home, quote pages

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -29,36 +29,39 @@ function Calculator() {
     return 0;
   };
   return (
-    <div className="wrapper">
-      <input className="input-field" readOnly value={display()} type="text" maxLength="0" />
-      <div className="btn-wrapper">
-        <Button value="AC" className="btn" onEvent={handler} />
-        <Button value="+/-" className="btn" onEvent={handler} />
-        <Button value="%" className="btn" onEvent={handler} />
-        <Button value="÷" className="btn light" onEvent={handler} />
-      </div>
-      <div className="btn-wrapper">
-        <Button value="7" className="btn" onEvent={handler} />
-        <Button value="8" className="btn" onEvent={handler} />
-        <Button value="9" className="btn" onEvent={handler} />
-        <Button value="x" className="btn light" onEvent={handler} />
-      </div>
-      <div className="btn-wrapper">
-        <Button value="4" className="btn" onEvent={handler} />
-        <Button value="5" className="btn" onEvent={handler} />
-        <Button value="6" className="btn" onEvent={handler} />
-        <Button value="-" className="btn light" onEvent={handler} />
-      </div>
-      <div className="btn-wrapper">
-        <Button value="1" className="btn" onEvent={handler} />
-        <Button value="2" className="btn" onEvent={handler} />
-        <Button value="3" className="btn" onEvent={handler} />
-        <Button value="+" className="btn light" onEvent={handler} />
-      </div>
-      <div className="btn-wrapper">
-        <Button value="0" className="btn wide" onEvent={handler} />
-        <Button value="." className="btn" onEvent={handler} />
-        <Button value="=" className="btn light" onEvent={handler} />
+    <div className="same">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="wrapper">
+        <input className="input-field" readOnly value={display()} type="text" maxLength="0" />
+        <div className="btn-wrapper">
+          <Button value="AC" className="btn" onEvent={handler} />
+          <Button value="+/-" className="btn" onEvent={handler} />
+          <Button value="%" className="btn" onEvent={handler} />
+          <Button value="÷" className="btn light" onEvent={handler} />
+        </div>
+        <div className="btn-wrapper">
+          <Button value="7" className="btn" onEvent={handler} />
+          <Button value="8" className="btn" onEvent={handler} />
+          <Button value="9" className="btn" onEvent={handler} />
+          <Button value="x" className="btn light" onEvent={handler} />
+        </div>
+        <div className="btn-wrapper">
+          <Button value="4" className="btn" onEvent={handler} />
+          <Button value="5" className="btn" onEvent={handler} />
+          <Button value="6" className="btn" onEvent={handler} />
+          <Button value="-" className="btn light" onEvent={handler} />
+        </div>
+        <div className="btn-wrapper">
+          <Button value="1" className="btn" onEvent={handler} />
+          <Button value="2" className="btn" onEvent={handler} />
+          <Button value="3" className="btn" onEvent={handler} />
+          <Button value="+" className="btn light" onEvent={handler} />
+        </div>
+        <div className="btn-wrapper">
+          <Button value="0" className="btn wide" onEvent={handler} />
+          <Button value="." className="btn" onEvent={handler} />
+          <Button value="=" className="btn light" onEvent={handler} />
+        </div>
       </div>
     </div>
   );
